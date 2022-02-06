7d4b54870ef5d90ebcea5a0fb72a403df6c41f73
Fabio Fernandes <boicotinho@gmail.com>
2022-02-06 19:58:38 +0200

    bring back ut_basics

--- web_socket_test/test_async_echo.cpp
+++ web_socket_test/test_async_echo.cpp
@@ -1,17 +1,3 @@
-/*
-#define BOOST_TEST_MODULE web_socket_test
-#include <boost/test/included/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(wss)
-
-BOOST_AUTO_TEST_CASE(simple_tls_request)
-{
-    BOOST_CHECK_LT(2, 1);
-}
-
-BOOST_AUTO_TEST_SUITE_END()
-*/
-/*
 #define USE_REAL_CERTIFICATES 0
 
 #include <boost/version.hpp> // 1.76
@@ -314,4 +300,3 @@ int main(int argc, char** argv)
 
     return EXIT_SUCCESS;
 }
-*/
---
+++ web_socket_test/ut_basics.cpp
@@ -0,0 +1,10 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(wss)
+
+BOOST_AUTO_TEST_CASE(basics)
+{
+    BOOST_CHECK_LT(1, 2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
