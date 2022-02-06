ce2c1fbe5cf20f9e14f37c2b74a08c0bc4e690d7
Fabio Fernandes <boicotinho@gmail.com>
2022-02-06 23:59:08 +0200

    experimenting with feed latencies

--- web_socket_test/ut_synchronous_bitfinex.cpp
+++ web_socket_test/ut_synchronous_bitfinex.cpp
@@ -11,6 +11,12 @@
 #include <iostream>
 #include <string>
 
+#include <atomic>
+#include <thread>
+#include <x86intrin.h>
+
+BOOST_AUTO_TEST_SUITE(wss)
+
 namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
 namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
@@ -18,74 +24,172 @@ namespace net = boost::asio;            // from <boost/asio.hpp>
 namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
-BOOST_AUTO_TEST_SUITE(wss)
+#define COMPILER_BARRIER()  __asm__ __volatile("")
 
-BOOST_AUTO_TEST_CASE(synchronous_bitfinex)
+uint64_t rdtscp()
 {
-    auto const host = "api-pub.bitfinex.com";
-    auto const port = "443";
-    auto const rpcJson = R"({ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD" })";
-    auto const hshk = "/ws/2";
+    uint32_t aux;
+    COMPILER_BARRIER();
+    uint64_t res = __rdtscp(&aux);
+    COMPILER_BARRIER();
+    return res;
+}
 
-    // The io_context is required for all I/O
-    net::io_context ioc;
+class TestClient
+{
+    using WebSocket = websocket::stream<beast::ssl_stream<tcp::socket>>;
+    net::io_context m_ioc;
+    ssl::context    m_ctx       {ssl::context::tlsv12_client};
+    tcp::resolver   m_resolver  {m_ioc};
+    WebSocket       m_ws        {m_ioc, m_ctx};
+public:
+    TestClient( const std::string& host,
+                const std::string& port,
+                const std::string& hshk )
+    {
+        // Look up the domain name
+        auto const results = m_resolver.resolve(host, port);
 
-    // The SSL context is required, and holds certificates
-    ssl::context ctx{ssl::context::tlsv12_client};
+        // Make the connection on the IP address we get from a lookup
+        net::connect(m_ws.next_layer().next_layer(), results.begin(), results.end());
 
-    // This holds the root certificate used for verification
-    //load_root_certificates(ctx);
+        // Perform the SSL handshake
+        m_ws.next_layer().handshake(ssl::stream_base::client);
 
-    // These objects perform our I/O
-    tcp::resolver resolver{ioc};
-    websocket::stream<beast::ssl_stream<tcp::socket>> ws{ioc, ctx};
+        // Set a decorator to change the User-Agent of the handshake
+        m_ws.set_option(websocket::stream_base::decorator(
+            [](websocket::request_type& req)
+            {
+                req.set(http::field::user_agent,
+                    std::string(BOOST_BEAST_VERSION_STRING) +
+                        " websocket-client-coro");
+            }));
 
-    // Look up the domain name
-    auto const results = resolver.resolve(host, port);
+        // Perform the websocket handshake
+        m_ws.handshake(host, hshk);
+    }
 
-    // Make the connection on the IP address we get from a lookup
-    net::connect(ws.next_layer().next_layer(), results.begin(), results.end());
+    ~TestClient()
+    {
+        beast::error_code ec;
+        m_ws.close(websocket::close_code::normal, ec); // none, normal
+    }
 
-    // Perform the SSL handshake
-    ws.next_layer().handshake(ssl::stream_base::client);
+    WebSocket& ws() {return m_ws;}
+};
 
-    // Set a decorator to change the User-Agent of the handshake
-    ws.set_option(websocket::stream_base::decorator(
-        [](websocket::request_type& req)
-        {
-            req.set(http::field::user_agent,
-                std::string(BOOST_BEAST_VERSION_STRING) +
-                    " websocket-client-coro");
-        }));
+using CountDownVal = std::atomic<int>;
 
-    // Perform the websocket handshake
-    ws.handshake(host, hshk);
+struct Args
+{
+    Args() = default;
+    Args(const std::string& jrpc_req, CountDownVal* cnt) : req(jrpc_req), p_countdown(cnt) {}
+    bool print       {false};
+    bool timestamps  {true};
+    size_t num_recvs {2};
+    std::string host {"api-pub.bitfinex.com"};
+    std::string port {"443"};
+    std::string hshk {"/ws/2"};
+    std::string req  {R"({ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD" })"};
+    // Sync multi-threaded startup
+    CountDownVal* p_countdown {};
+    // Work Result
+    std::vector<uint64_t> ts;
+    beast::flat_buffer result_buffer;
+};
+
+void RunTest(Args& args)
+{
+    TestClient client(args.host, args.port, args.hshk);
+
+    if(args.p_countdown)
+    {
+        -- *args.p_countdown;
+        while(args.p_countdown->load())
+            _mm_pause();
+    }
 
     // Our message in this case should be stringified JSON-RPC request
-    ws.write(net::buffer(std::string(rpcJson)));
+    client.ws().write(net::buffer(args.req));
 
     // This buffer will hold the incoming message
-    beast::flat_buffer buffer;
+    args.result_buffer.reserve(100000 + args.num_recvs * 64);
+
+    args.ts.reserve(args.num_recvs);
 
     // Read a message into our buffer
     ssize_t rd = 0;
 
-    for(int ii = 0; ii < 10; ++ii)
+    for(size_t ii = 0; ii < args.num_recvs; ++ii)
     {
-        rd = ws.read(buffer);
+        rd = client.ws().read(args.result_buffer);
+        const auto now = rdtscp();
         if(rd <= 0)
         {
             break;
         }
-        auto const segment = buffer.cdata();
-        std::cout << "rd=" << rd << "; '" << beast::make_printable(segment) << "'\n\n";
-        buffer.consume(segment.size());
+        if(args.print)
+        {
+            auto const segment = args.result_buffer.cdata();
+                std::cout << now << " rd=" << rd << "; '" << beast::make_printable(segment) << "'\n";
+            args.result_buffer.consume(segment.size());
+        }
+        if(rd < 200 && args.timestamps)
+        {
+            args.ts.push_back(now);
+        }
     }
-
-    // Close the WebSocket connection
-    beast::error_code ec;
-    ws.close(websocket::close_code::normal, ec); // none, normal
-    //std::cout << "rd=" << rd << "; " <<  beast::make_printable(buffer.data()) << std::endl;
+    //std::cout << beast::make_printable(args.result_buffer.data()) << std::endl;
 }
+
+BOOST_AUTO_TEST_CASE(synchronous_bitfinex_P0)
+{
+    Args args(R"({ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD" })", nullptr);
+    args.print = true;
+    RunTest(args);
+}
+
+BOOST_AUTO_TEST_CASE(synchronous_bitfinex_R0)
+{
+    Args args(R"({ "event": "subscribe", "channel": "book", "prec": "R0", "symbol": "tBTCUSD" })", nullptr);
+    args.print = true;
+    RunTest(args);
+}
+
+BOOST_AUTO_TEST_CASE(synchronous_bitfinex_both)
+{
+    Args args(R"({ "event": "subscribe", "channel": "book", "prec": "R0", "symbol": "tBTCUSD", "subId":"888" }{ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD", "subId":"999" })", nullptr);
+    args.print = true;
+    RunTest(args);
+}
+
+BOOST_AUTO_TEST_CASE(R0_P0_perf_compare)
+{
+    CountDownVal ready_countdown {2};
+
+    Args args_p0(R"({ "event": "subscribe", "channel": "book", "symbol": "tBTCUSD" })", &ready_countdown);
+    Args args_r0(R"({ "event": "subscribe", "channel": "book", "prec": "R0", "symbol": "tBTCUSD" })", &ready_countdown);
+
+    // args_p0.print = true;
+    // args_r0.print = true;
+    // args_p0.timestamps = false;
+    // args_r0.timestamps = false;
+
+    std::thread thread_p0(RunTest, std::ref<Args>(args_p0));
+    std::thread thread_r0(RunTest, std::ref<Args>(args_r0));
+
+    thread_p0.join();
+    thread_r0.join();
+    std::vector<int64_t> diff; // when is feed r0 faster than p0?
+
+    const size_t sz = std::min(args_p0.ts.size(), args_r0.ts.size());
+    for(size_t ii = 0; ii < sz; ++ii)
+    {
+        const int64_t dd = args_r0.ts[ii] - args_p0.ts[ii];
+        std::cout << "Diff: " << dd << "\n";
+        diff.push_back(dd);
+    }
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
