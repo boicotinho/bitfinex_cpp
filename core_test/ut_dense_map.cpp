4bf4c9228ae2e4f49e1325cded7d354182e1ddc8
Fabio Fernandes <boicotinho@gmail.com>
2022-02-09 19:44:26 +0200

    dense_map benchmark

--- core_test/ut_dense_map.cpp
+++ core_test/ut_dense_map.cpp
@@ -3,9 +3,40 @@
 #include "core/dense_map.h"
 #include "core/hashers.h"
 
-#if (DENSE_MAP_SUPPORTED)
+inline boost::test_tools::assertion_result exec_microbench(boost::unit_test::test_unit_id)
+{
+    #ifdef NDEBUG
+        return true;
+    #else
+        return false;
+    #endif
+}
 
 BOOST_AUTO_TEST_SUITE(core)
+
+// For Profiling
+constexpr int NN = 1000;
+constexpr int QQ = 988; // 988
+constexpr int SS = 1031;
+
+std::vector<int> const g_random_keys = []()
+{
+    std::vector<int> ret;
+    for(int ii = 0; ii < QQ; ++ii)
+    {
+        const int kk = std::rand();
+        ret.push_back(kk);
+    }
+    return ret;
+}();
+
+size_t GenKey(size_t xx)
+{
+    //return std::hash<size_t>()(xx) % QQ;
+    return g_random_keys[xx % g_random_keys.size()];
+}
+
+#if (DENSE_MAP_SUPPORTED)
 
 BOOST_AUTO_TEST_CASE(dense_map)
 {
@@ -17,10 +48,10 @@ BOOST_AUTO_TEST_CASE(dense_map)
     struct TagToBookMapTraits
     {
         using Key               = uint32_t;
+        using Value             = uint32_t;
         using Hasher            = IdentityHasher<Key>;
         enum { EMPTY_KEY        = 0 };
         enum { MIN_NUM_BUCKETS  = 32  };
-        using Value             = uint32_t;
     };
 
     DenseMap<TagToBookMapTraits> map;
@@ -54,6 +85,93 @@ BOOST_AUTO_TEST_CASE(dense_map)
     BOOST_CHECK_EQUAL(*pval, 333);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_CASE(profile_dense_map, *boost::unit_test::precondition(exec_microbench))
+{
+    using ValType = uint16_t;
+    using KeyType = uint16_t;
 
+    struct TagToBookMapTraits
+    {
+        using Key               = KeyType;
+        using Value             = ValType;
+        using Hasher            = IdentityHasher<Key>;
+        enum { EMPTY_KEY        = 0 };
+        enum { MIN_NUM_BUCKETS  = SS };
+    };
+
+    DenseMap<TagToBookMapTraits> map;
+
+    for(int qq = 0; qq < QQ; ++qq)
+    {
+        auto const kk = GenKey(qq);
+        map.emplace(kk, qq);
+    }
+
+    std::vector<CpuTimeStamp> timings;
+    timings.reserve(NN);
+
+    for(int ii = 0; ii < NN; ++ii)
+    {
+        auto const kk = GenKey(ii);
+        ValType const* vv;
+        auto const t0 = rdtscp();
+        vv = map.lookup_ptr(kk);
+        auto const t1 = rdtscp();
+        volatile auto dummy = *vv;
+        timings.push_back(t1 - t0);
+    }
+
+    std::cout << FormatCcTimingsTable(timings, "DenseMap lookup hit");
+}
 #endif // DENSE_MAP_SUPPORTED
+
+BOOST_AUTO_TEST_CASE(profile_unordered_map, *boost::unit_test::precondition(exec_microbench))
+{
+    using ValType = uint16_t;
+    using KeyType = uint16_t;
+
+    std::unordered_map<KeyType,ValType> map;
+    map.reserve(QQ);
+
+    for(int qq = 0; qq < QQ; ++qq)
+    {
+        auto const kk = GenKey(qq);
+        map.emplace(kk, qq);
+    }
+
+    std::cout << "size:    " << map.size() << "\n";
+    std::cout << "buckets: " << map.bucket_count() << "\n";
+    std::cout << "ld_fact: " << map.load_factor() << "\n";
+    std::cout << "\n";
+
+    std::vector<CpuTimeStamp> timings;
+    timings.reserve(NN);
+
+    for(int ii = 0; ii < NN; ++ii)
+    {
+        auto const kk = GenKey(ii);;
+        ValType const* vv;
+        auto const t0 = rdtscp();
+        auto it = map.find(kk);
+        vv = &it->second;
+        auto const t1 = rdtscp();
+        volatile auto dummy = *vv;
+        timings.push_back(t1 - t0);
+    }
+
+    std::cout << FormatCcTimingsTable(timings, "std::unordered_map lookup hit");
+    // Perf 1,000 x std::unordered_map lookup hit
+    //     0.0 % :           54 cc
+    //    10.0 % :           60 cc
+    //    50.0 % :           81 cc
+    //    75.0 % :          120 cc
+    //    90.0 % :          147 cc
+    //    99.0 % :          231 cc
+    //    99.5 % :          276 cc
+    //    99.8 % :          342 cc
+    //    99.9 % :          381 cc
+    //   100.0 % :          381 cc
+    //   average :           94 cc
+}
+
+BOOST_AUTO_TEST_SUITE_END()
