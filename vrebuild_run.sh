6eadb8275fb237b8e3eb08b5131a8360ab8e7e47
Fabio <fabio@debian2022.com>
2022-04-15 17:50:02 +0300

    rebuild runs test_poc_sarb instead of bitfinex

--- vrebuild_run.sh
+++ vrebuild_run.sh
@@ -3,4 +3,5 @@ CFG=${1:-Release}
 rm -rf build_${CFG}
 ./vbuild.sh ${CFG}
 cd build_${CFG}
-marketlinks/bitfinex/utest/bitfinex_utest
+#marketlinks/bitfinex/utest/bitfinex_utest
+marketlinks/binance/test_poc_sarb/test_poc_sarb
